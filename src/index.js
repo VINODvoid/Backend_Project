f232919141bbf4dc55cf3f56bbc770948a1bce3b
VINODvoid <iamlearner.v@gmail.com>
2024-02-16 21:30:04 +0530

    build :  added db-connection scripts

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-console.log("hello world!");
+// require("dotenv").config();  // ! It conflicts as we are using es6 import
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+
+dotenv.config(
+    {
+        path:"./env"
+    }
+);
+connectDB();
+
+
+
+
+
+
+
+
+
+// * First Approach to connect to the database , start the server  and handle errors
+/* const app = express();
+(async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) =>{
+            console.log("ERROR: ", error);
+            throw error;
+        })
+        app.listen(process.env.PORT, () =>{
+            console.log(`Server is running on port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.log("ERROR: ", error);
+        throw error;
+    }
+})() */
